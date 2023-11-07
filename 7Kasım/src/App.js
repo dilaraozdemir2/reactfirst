e2f3546261def02c05bf14aa1cdd0c56b7eb98fa
dilaraozdemir2 <94180093+dilaraozdemir2@users.noreply.github.com>
2023-11-07 16:19:47 +0300

    ilkdeneme

---
+++ 7Kasım/src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    // <!-- Image and text -->
+    <div>
+      <nav class="navbar navbar-light bg-light">
+        <a class="navbar-brand" href="#">
+          <img src="https://i.pinimg.com/1200x/0d/cf/b5/0dcfb548989afdf22afff75e2a46a508.jpg" width="30" height="30" class="d-inline-block align-top" alt="" />
+        FLYING EAGLE
+        </a>
+        <a href="#" class="badge badge-dark" className="link">Anasayfa</a>
+        <a href="#" class="badge badge-dark" className="link">Galeri</a>
+        <a href="#" class="badge badge-dark" className="link">İçindekiler</a>
+        <a href="#" class="badge badge-dark" className="link">Hakkında</a>  
+      </nav>
+
+
+    </div>
+  );
+}
+
+export default App;
