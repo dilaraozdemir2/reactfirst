f30d5084c4bfa0dfcef521dcb9c7e0d7a50ddf2a
dilaraozdemir2 <94180093+dilaraozdemir2@users.noreply.github.com>
2023-11-08 10:28:22 +0300

    Counter
    Bir counter tasarladım. Sayfanın en üstünde counter isimli bir başlık ekledim. Sonrasında arttırma ve azaltma butonları ekledim. Butonların arasına ise mevcut sayının kaç olduğunu güncel olarak gösteren bir değer koydum.
    Arttırıma ve azaltma butonlarının çalışmasını sağlayan fonksiyonlar tanımladım ve bu fonksiyonları koşullara bağladım. Sayı değerini de Usestate kullanarak belirledim.

---
+++ 8KasımFirst/src/Counter.js
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+
+function Counter() {
+    const [sayi, setsayi] = useState(0)
+    // useState ile başlangıçta ne olacağını belirliyoruz, bu react ta olan bir yapı ve kullanmak için öncesinde burada tanımladık yazıp enterlayınca yukarıda import etti
+    //  sayi değerini setsayi ile değiştiriyoruz 
+    const arttırma1 = () => {
+        setsayi(sayi + 2);
+        //değeri belli olan bir fonksiyon ok kullanılıyor diye adı arrow function önemli değil
+    }
+
+    function arttırma() {
+        if (sayi < 10) {
+            setsayi(sayi + 1);
+            //setsayi da aslında bir fonksiyon
+        }
+    }
+    function azaltma() {
+        if (sayi > 0) {
+            setsayi(sayi - 1);
+        }
+
+    }
+
+
+    return (
+        <div>
+            <h1>Counter</h1>
+            <div>
+                <button onClick={arttırma}>Arttır</button>
+                <p>{sayi}</p>
+                <button onClick={azaltma}>Azalt</button>
+            </div>
+        </div>
+    );
+}
+
+export default Counter;
